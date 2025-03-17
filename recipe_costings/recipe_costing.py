795144d162542948c5da28d7c9cc68e311c1d78f
Barrie Seldon <barrieseldon@gmail.com>
2025-03-17 12:14:09 +0800

    change to float from int and added round

--- recipe_costings/recipe_costing.py
+++ recipe_costings/recipe_costing.py
@@ -1,6 +1,6 @@
 recipe = input("Name of Recipe> ")
 
-portions = input("Number of Portions> ")
+portions = int(input("Number of Portions> "))
 
 ingredient_cost_list = []
 
@@ -9,10 +9,10 @@ while True:
     ingredient_weight = input("Total Weight(grams)> ")
     ingredient_cpg = (float(input("Ingredient Cost($)/kilo> ")) / 1000)
     reply = input("Keep going? (y/n)> ")
-    total_cost = (int(ingredient_weight) * float(ingredient_cpg))
-    ingredient_cost_list.append(int(total_cost))
+    total_cost = (float(ingredient_weight) * float(ingredient_cpg))
+    ingredient_cost_list.append(float(total_cost))
     if reply == "n":
         break
 
 
-print("$" + str(sum(ingredient_cost_list) / int(portions)) + " per portion")
+print(("$" + str(round((sum(ingredient_cost_list) / portions) , 2)) + " per portion"))
