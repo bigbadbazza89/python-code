a4c02ca964ceeddc7538de981338240e58323833
Barrie Seldon <barrieseldon@gmail.com>
2025-03-17 11:15:22 +0800

    updated loop

--- recipe_costings/recipe_costing.py
+++ recipe_costings/recipe_costing.py
@@ -5,13 +5,14 @@ portions = input("Number of Portions> ")
 ingredient_cost_list = []
 
 while True:
-    ingredient_name = input("Ingredient" + "\x1b[3;37;49m (type 'no' to end)\x1b[0m" + ">")
-    if ingredient_name == "no":
-        break
+    ingredient_name = input("Ingredient> ")
     ingredient_weight = input("Total Weight(grams)> ")
     ingredient_cpg = (float(input("Ingredient Cost($)/kilo> ")) / 1000)
+    reply = input("Keep going? (y/n)> ")
     total_cost = (int(ingredient_weight) * float(ingredient_cpg))
     ingredient_cost_list.append(int(total_cost))
+    if reply == "n":
+        break
+
 
 print("$" + str(sum(ingredient_cost_list) / int(portions)) + " per portion")
-
