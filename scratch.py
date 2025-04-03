8303cabfb8b4c2bdf176a6a7334aa06dd405e10e
Barrie Seldon <barrieseldon@gmail.com>
2025-04-03 19:27:57 +0800

    removed scratch 2 into scratch 1

--- scratch.py
+++ scratch.py
@@ -1,1 +1,6 @@
-import time
+# words = list('kjshdfkjhsdfkhsdkfjh')
+# print(words[-10:-3])
+
+bikes = [1, "red", 6.7]
+for object in bikes:
+    print(f"I love {object}")
--- scratch2.py
+++
@@ -1,6 +0,0 @@
-# words = list('kjshdfkjhsdfkhsdkfjh')
-# print(words[-10:-3])
-
-bikes = [1, "red", 6.7]
-for object in bikes:
-    print(f"I love {object}")
